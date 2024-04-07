c88a796aee5348827859fcfdcad5d5ad2a516b97
rahuljha906 <jha.rahul906@gmail.com>
2024-04-07 13:36:10 +0530

    rahul.jha|Reverse words in sentence.

---
+++ rahul/jha/data/structure/array/ReverseWord.java
@@ -0,0 +1,46 @@
+package rahul.jha.data.structure.array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+Given an input string s, reverse the order of the words.
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+Return a string of the words in reverse order concatenated by a single space.
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+Example 1:
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+Constraints:
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+ */
+public class ReverseWord {
+    public static void main(String[] args) {
+        String str = "hello word";
+        reverseWord(str);
+        System.out.println(reverseWord(str));
+    }
+
+    static String reverseWord(String str) {
+        String a[] = str.split(" ");
+        List<String> stList = Arrays.stream(a).collect(Collectors.toList());
+        Collections.reverse(stList);
+        return stList.stream().collect(Collectors.joining(" "));
+    }
+
+}
+
+
